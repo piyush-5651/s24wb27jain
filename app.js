7773e13e31c7dfe29db01665051d14191174f365
piyush-5651 <s565127@nwmissouri.edu>
2024-03-27 21:12:38 -0500

    with bonus

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var raftboatRouter = require('./routes/raftboat');
 var gridRouter = require('./routes/grid');
+var randomItemRouter = require('./routes/pick');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/raftboat', raftboatRouter);
 app.use('/grid',gridRouter);
+app.use('/randomitem', randomItemRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
